7cd61beebcf49ca80701cce9dafb90afdb66f7a0
austinhteng <austinhteng@gmail.com>
2022-03-01 17:11:21 -0600

    PhiFlow simulator & reader

---
+++ data/sim_000000/src/empty-fluid-simulator.py
@@ -0,0 +1,29 @@
+from phi.flow import *
+
+scene = Scene.create('./data/')
+
+bounds=Box[0:32, 0:40]
+GRAVITY = math.tensor([0, -9.81])
+DT = 1
+
+water_ref = CenteredGrid(Noise(), extrapolation.BOUNDARY, x=32, y=40, bounds=bounds)
+velocity_ref = StaggeredGrid(0, extrapolation.ZERO, x=32, y=40, bounds=bounds)
+#velocity_ref = StaggeredGrid(Noise(), extrapolation.ZERO, x=32, y=40, bounds=bounds) * 10
+vis.plot([water_ref, velocity_ref])
+
+water = water_ref
+velocity = velocity_ref
+
+#add gravity
+gravity_force = GRAVITY * DT
+#gravity_force = 0
+velocity += gravity_force
+velocity, _ = fluid.make_incompressible(velocity)
+
+for i in range(20):
+  water = advect.mac_cormack(water, velocity, dt=DT)
+  gravity_force = water * GRAVITY * DT @ velocity
+  velocity = advect.semi_lagrangian(velocity, velocity, dt=DT) + gravity_force
+  velocity, _ = fluid.make_incompressible(velocity)
+
+  scene.write({'water' : water, 'velocity' : velocity}, fames=i)
